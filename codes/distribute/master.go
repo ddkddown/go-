5a162f156ed0bcbff289d3162af713c1bcccd493
ddkddown <2628397115@qq.com>
2021-04-15 21:54:52 +0800

    [update] update master.go

--- codes/distribute/master.go
+++ codes/distribute/master.go
@@ -7,10 +7,16 @@ import (
 	"net/http"
 	"net/rpc"
 	"os"
+	"sync"
 )
 
-type SrcFiles map[string]bool
-type MiddleFiles map[string]bool
+type check struct {
+	flag   bool
+	worker string
+}
+
+type SrcFiles map[string]check
+type MiddleFiles map[string]check
 type WorkerInfo map[string]int
 
 type Master struct {
@@ -18,9 +24,14 @@ type Master struct {
 	srcs    SrcFiles    //源文件
 	middles MiddleFiles //中间文件
 	workers WorkerInfo  //workers名称
+	mu      sync.Mutex  //mutex
 }
 
+//Regist worker regist
 func (p *Master) Regist(request RegistReq) error {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+
 	if _, exist := p.workers[request.workerName]; exist {
 		return errors.New("worker already registed!")
 	}
@@ -29,17 +40,36 @@ func (p *Master) Regist(request RegistReq) error {
 	return nil
 }
 
-func (p *Master) AskTask(request AskTaskReq, response *AskTaskRsp) int, error {
+//AskTask worker fetch task
+func (p *Master) AskTask(request AskTaskReq, response *AskTaskRsp) (int, error) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+
 	if _, exist := p.workers[request.workerName]; !exist {
 		return -1, errors.New("worker not registed!")
 	}
 
 	for k, v := range p.srcs {
-		if v {
+		if v.flag {
 			continue
 		}
 
 		response.fileName = k
+		v.flag = true
+		v.worker = request.workerName
+		p.srcs[k] = v
+		return 0, nil
+	}
+
+	for k, v := range p.middles {
+		if v.flag {
+			continue
+		}
+
+		response.fileName = k
+		v.flag = true
+		v.worker = request.workerName
+		p.middles[k] = v
 		return 0, nil
 	}
 
@@ -79,10 +109,40 @@ func (m *Master) server() {
 // if the entire job has finished.
 //
 func (m *Master) Done() bool {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
 	ret := false
 
 	// Your code here.
+	if 0 == len(m.srcs) && 0 == len(m.middles) {
+		ret = true
+		return ret
+	}
 
+	for w, c := range m.workers {
+		c++
+		if c >= 10 {
+			// worker down
+			for k, v := range m.srcs {
+				if v.worker == w {
+					v.flag = false
+					v.worker = ""
+					m.srcs[k] = v
+				}
+			}
+
+			for k, v := range m.middles {
+				if v.worker == w {
+					v.flag = false
+					v.worker = ""
+					m.middles[k] = v
+				}
+			}
+		} else {
+			m.workers[w] = c
+		}
+	}
 	return ret
 }
 
@@ -95,6 +155,9 @@ func MakeMaster(files []string, nReduce int) *Master {
 	m := Master{}
 
 	// Your code here.
+	m.srcs = make(map[string]check)
+	m.middles = make(map[string]check)
+	m.workers = make(map[string]int)
 
 	m.server()
 	return &m
