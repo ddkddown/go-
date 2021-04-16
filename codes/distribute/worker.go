a82bc794d0ff391f308e5cbf10729cccec3e22a0
ddk <2628397115@qq.com>
2021-04-16 11:06:40 +0800

    Update worker.go

--- codes/distribute/worker.go
+++ codes/distribute/worker.go
@@ -1,10 +1,13 @@
 package mr
 
-import "fmt"
-import "log"
-import "net/rpc"
-import "hash/fnv"
-
+import (
+	"fmt"
+	"hash/fnv"
+	"io/ioutil"
+	"log"
+	"net/rpc"
+	"os"
+)
 
 //
 // Map functions return a slice of KeyValue.
@@ -24,7 +27,6 @@ func ihash(key string) int {
 	return int(h.Sum32() & 0x7fffffff)
 }
 
-
 //
 // main/mrworker.go calls this function.
 //
@@ -35,7 +37,52 @@ func Worker(mapf func(string, string) []KeyValue,
 
 	// uncomment to send the Example RPC to the master.
 	// CallExample()
+	regisReq := RegistReq{"worker1"}
+	t := 0
+	call("Master.Regist", &regisReq, t)
 
+	//for {
+	srcReq := AskSrcTaskReq{"worker1"}
+	var srcRsp AskSrcTaskRsp
+	if call("Master.AskSrcTask", &srcReq, &srcRsp) {
+		fmt.Println("task name: ", srcRsp.FileName)
+	}
+	//}
+	intermediate := []KeyValue{}
+	file, err := os.Open(srcRsp.FileName)
+	if err != nil {
+		log.Fatalf("cannot open %v", srcRsp.FileName)
+	}
+	content, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Fatalf("cannot read %v", srcRsp.FileName)
+	}
+	file.Close()
+	kva := mapf(srcRsp.FileName, string(content))
+	intermediate = append(intermediate, kva...)
+
+	oname := "mr-out-0"
+	ofile, _ := os.Create(oname)
+
+	i := 0
+	for i < len(intermediate) {
+		j := i + 1
+		for j < len(intermediate) && intermediate[j].Key == intermediate[i].Key {
+			j++
+		}
+		values := []string{}
+		for k := i; k < j; k++ {
+			values = append(values, intermediate[k].Value)
+		}
+		output := reducef(intermediate[i].Key, values)
+
+		// this is the correct format for each line of Reduce output.
+		fmt.Fprintf(ofile, "%v %v\n", intermediate[i].Key, output)
+
+		i = j
+	}
+
+	ofile.Close()
 }
 
 //
