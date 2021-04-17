f59bc5e6bd68f04d0b6470cac287e33dc2ebcc31
ddkddown <2628397115@qq.com>
2021-04-17 21:05:09 +0800

    [update] new files

---
+++ codes/intset/intset.go
@@ -0,0 +1,11 @@
+package main
+
+type IntSet struct {
+	word []uint64
+}
+
+func (s *IntSet) Has(x int) bool {
+	word, bit := x/64, uint(x%64)
+	return word < len(s.words) && s.words[word]&(1<<bit) != 0
+}
+
