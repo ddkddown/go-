bd6de3c4eda518301d1b6982428c3e88fc105f1e
dongdakuan1466@ichinae.com <2628397115@qq.com>
2021-05-31 09:58:35 +0800

    [update] update new file

--- codes/proj/src/grpc_server/main.go
+++ codes/proj/src/grpc_server/main.go
@@ -1,13 +1,26 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
 	"log"
 	"net"
 
 	"ddk/myproto"
 
+	"net/http"
+
 	"google.golang.org/grpc"
 )
+
+type RegisReq struct {
+	Id      string   `json:"id"`
+	Name    string   `json:"name"`
+	Address string   `json:"address"`
+	Port    int      `json:"port"`
+	Tags    []string `json:"tags"`
+}
 
 func main() {
 	grpcServer := grpc.NewServer()
@@ -17,6 +30,32 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	regis := &RegisReq{
+		Id:      "grpc_test",
+		Name:    "grpc",
+		Address: "192.168.10.210",
+		Port:    1234,
+		Tags:    []string{"grpc_server"},
+	}
+
+	client := &http.Client{}
+
+	json, err := json.Marshal(regis)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req, err := http.NewRequest(http.MethodPut, "http://192.168.10.210:8500/v1/agent/service/register", bytes.NewBuffer(json))
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Set("Content-Type", "application/json; charset=utf-8")
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(resp.StatusCode)
 
 	grpcServer.Serve(lis)
 }
