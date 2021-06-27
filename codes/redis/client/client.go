7b67107c1fde8765d282e4bacb6d4b2f0f30d9dd
ddkddown <2628397115@qq.com>
2021-06-27 11:59:30 +0800

    [update] new file

--- codes/redis/client/client.go
+++ codes/redis/client/client.go
@@ -2,11 +2,12 @@ package client
 
 import (
 	"context"
+	"fmt"
+	"strconv"
+	"time"
 
 	"github.com/go-redis/redis/v8"
 )
-
-var ctx = context.Background()
 
 var Rdb *redis.Client
 
@@ -16,4 +17,40 @@ func init() {
 		Password: "",
 		DB:       0,
 	})
+}
+
+func Lock(ctx context.Context, expireTime int64) (now int64, ret bool) {
+	now = time.Now().Unix()
+	ret, err := Rdb.SetNX(ctx, "mutex", now, time.Duration(expireTime)*time.Second).Result()
+
+	if !ret {
+		fmt.Println("lock mutex failed", err)
+		return
+	}
+
+	fmt.Println("lock succ")
+	return
+}
+
+func Unlock(ctx context.Context, timeStamp int64) (ret int) {
+	val, _ := Rdb.Get(ctx, "mutex").Result()
+	i, _ := strconv.ParseInt(val, 10, 64)
+	if timeStamp == i {
+		if del, _ := Rdb.Del(ctx, "mutex").Result(); 1 != del {
+			fmt.Printf("delete mutex failed: %d", del)
+			return -1
+		}
+
+		fmt.Println("unlock succ:")
+		return 0
+
+	}
+	if 0 == i {
+		fmt.Println("mutex auto released!")
+		return 0
+	}
+
+	fmt.Println("timeStamp not eq ", timeStamp, i)
+
+	return 1
 }
--- codes/redis/main.go
+++ codes/redis/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"time"
 
 	"redis/client"
@@ -14,17 +13,23 @@ var (
 
 func main() {
 
-	err := client.Rdb.Set(ctx, "key", "value", 10).Err()
-	if err != nil {
-		panic(err)
-	}
+	/*
+			err := client.Rdb.Set(ctx, "key", "value", 10).Err()
+			if err != nil {
+				panic(err)
+			}
 
+			time.Sleep(12 * time.Second)
+
+			val, err := client.Rdb.Get(ctx, "key").Result()
+			if err != nil {
+				panic(err)
+			}
+
+		fmt.Println("wang:", val)
+	*/
+
+	now, _ := client.Lock(ctx, 10)
 	time.Sleep(12 * time.Second)
-
-	val, err := client.Rdb.Get(ctx, "key").Result()
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("wang:", val)
+	client.Unlock(ctx, now)
 }
